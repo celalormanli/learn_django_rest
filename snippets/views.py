7f771ddd4915dc2c219898819495a5bfb0cd9ef6
Celal ORMANLI <ormanlicelal@gmail.com>
2021-01-02 17:36:59 +0300

    change mixin

--- snippets/views.py
+++ snippets/views.py
@@ -93,25 +93,24 @@ def snippet_detail(request,pk, format=None):
 '''
 
 
-class SnippetList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+class SnippetList(generics.ListCreateAPIView):
     
     queryset=Snippet.objects.all()
     serializer_class=SnippetSerializer
-    
+    '''    
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
     
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
 
-class SnippetDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+    '''
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     
     queryset=Snippet.objects.all()
     serializer_class=SnippetSerializer
-   
+    '''
     def get(self, request, *args, **kwargs):
         return self.retrieve(request, *args, **kwargs)
     
@@ -119,4 +118,5 @@ class SnippetDetail(mixins.RetrieveModelMixin,
         return self.update(request, *args, **kwargs)
     
     def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
+        return self.destroy(request, *args, **kwargs) 
+    '''
